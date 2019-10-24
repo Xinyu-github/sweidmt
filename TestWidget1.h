724508625afb8ff188e42aa5ec2d67f29db68012
Xinyu-github <56533774+Xinyu-github@users.noreply.github.com>
2019-10-24 14:23:54 +0200

    Update TestWidget1.h
    main file?

--- TestWidget1.h
+++ TestWidget1.h
@@ -1,13 +1,4 @@
-/*
- *  Copyright (C) 2012 - 2013 Melanie Strake
- *
- *  Chair of Medical Engineering (mediTEC)
- *  RWTH Aachen University
- *  Pauwelsstr. 20
- *  52074 Aachen
- *  Germany
- *
- */
+
 
 #ifndef __TESTWIDGET1_H__
 #define __TESTWIDGET1_H__
@@ -24,30 +15,14 @@
 // TestWidget1
 // ----------------------------------------------------------------------------------
 
-/** \brief	Widget class of "Submenu 1"
-  *
-  * User finishes the processing of this submenu with a click on the push button "Click 1".
-  *
-  * \author	Melanie Strake
-  * \date	18.04.2013
-  */
+
 class TestWidget1: public QWidget
 {
 	Q_OBJECT
 
 public:
 
-	/** \brief	Instantiates...
-	  *
-	  * \param	data	application data
-	  * \param	widget	framework (parent widget)
-	  *
-	  * \note	\ref widget has to reference a widget from type \c QFramework, 
-	  *			if text message of the framework status box should be modified. 
-	  *			Otherwise the constructor can look like this: TestWidget1(CData *data, QWidget* widget = 0)
-	  *
-	  * \note	framework status box = QFrameworkDLL::QFramework::statusBox
-	  */
+	
 	TestWidget1(ApplicationData *data, QFramework* widget = 0);
 	
 
@@ -57,12 +32,7 @@ public:
 
 protected:
 
-	/** \brief Event handling
-	  *
-	  *	Reimplements handling of \c QEvent::Show and \c QEvent::Hide. 
-	  * \c QEvent::Show is posted in case of an entrance or re-entrance into submenu widget \ref TestWidget1. 
-	  * \c QEvent::Hide is posted in case of leaving submenu widget \ref TestWidget1 to enter and process another submenu widget. 
-	  */
+
 
 	bool event(QEvent* e);
 
@@ -92,21 +62,7 @@ private:
 
 private slots:
 
-	/** \brief	Called, when push button "Click 1" is clicked. Induces end of process, so that next submenu 
-	  *			can be entered and processed. In this case next submenu is "Submenu 2" (class \ref TestWidget2).
-	  *
-	  * In detail:
-	  *	Boolean value of \ref ApplicationData::widget1Finished is set to true. 
-	  * Important:  \c QFrameworkDLL::QWidgetEvent::FinishWorkingStep event is posted to framework widget. 
-	  * The status box content of the framework widget is updated with a confirmation message.
-	  *
-	  * \note	The status box content of the framework can also be used for general information and does not 
-	  *			have to be updated in every submenu widget. This is just an example. 
-	  *			Instead submenu widget \ref TestWidget1 can hold an own status box.
-	  *
-	  * \note	QFrameworkDLL::QWidgetEvent::FinishWorkingStep
-	  * \note	QFrameworkDLL::QStatusBox 
-	  */
+	
 	void on_load8bit_clicked();
 	void on_load12bit_clicked();
 	int fenster(int, int, int);
